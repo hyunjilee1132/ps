ae85c29662bff1bb58c4f7e7a390d6455b69cff0
hyunji <hyunji.lee1132@gmail.com>
2026-01-19 10:53:13 +0900

    [Silver III] Title: N과 M (2), Time: 192 ms, Memory: 18316 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Main {
+    static int n, m;
+    static int[] arr;
+    static int[] seq;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] input = sc.nextLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = i+1;
+        }
+
+        seq = new int[m];
+        visited = new boolean[n];
+
+        int idx = 0;
+        int start = 0;
+
+        permutation(idx, start);
+    }
+
+    static void permutation(int idx, int start) {
+        if(idx == m) {
+            for(int el : seq) {
+                System.out.print(el + " "); 
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=start; i<n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                seq[idx] = arr[i];
+                permutation(idx+1, i+1);
+                visited[i] = false;
+            }
+        }
+
+    }
+}
