31163950c831fb31f06a4bd616691cbaf473a31c
hyunji <hyunji.lee1132@gmail.com>
2026-01-25 22:50:22 +0900

    [Silver I] Title: IOIOI, Time: 436 ms, Memory: 29572 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -8,22 +8,23 @@ public class Main {
         String str = sc.next(); // 주어진 문자열 
 
         int count = 0;
+        int repeat = 0; 
 
-        StringBuilder ioi = new StringBuilder("I");
-        for (int i=0; i<n; i++) {
-            ioi.append("OI");
-        }
+        for (int i=1; i<m-1; i++) { // IOIOIOI
+            if (str.charAt(i-1)=='I' && str.charAt(i)=='O' && str.charAt(i+1)=='I') {
+                repeat++;
 
-        String strioi = new String(ioi); // sb -> string
-        
-        int len = strioi.length();
-        for (int i=0; i<=m-len; i++) {
-            if (str.substring(i, i+len).equals(strioi)) count++; 
+                if (repeat == n) {
+                    count++;
+                    repeat--; 
+                }
+                
+                i++;
+            } else {
+                repeat = 0;
+            }
         }
 
         System.out.println(count);
     }
 }
-
-// 비교할 문자열은 IOI에 for문 돌면서 OI 붙이고,
-// 그 값으로 주어지는 문자열에 슬라이딩 윈도우로 범위를 좁힌 뒤, equal한지 비교하기, 같으면 cnt++
